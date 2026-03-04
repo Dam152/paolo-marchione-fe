bb59f5dae94845134f22474b9715c5628aef4ecc
Damiano Ciucci <damjaguar98@icloud.com>
2026-03-04 16:08:12 +0100

    feat: refactor NotFound component to use styled components and improve layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,39 @@
 import Link from 'next/link';
+import { css, cx } from '../../panda/css';
+import { container } from '../../panda/patterns';
+import { Text } from '@/components/atoms/Text';
+import { NextLink } from '@/components/atoms/NextLink';
+import { text } from '../../panda/recipes';
+const styles = {
+  root: css({
+    w: '100%',
+    h: 'var(--root-height-less-header, 958px)',
+  }),
+
+  container: css({
+    h: 'inherit',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '24px',
+  }),
+};
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>404 — Page not found</h2>
-      <p>The page you are looking for does not exist.</p>
-      <Link href="/">Go back home</Link>
+    <div className={styles.root}>
+      <div className={cx(container(), styles.container)}>
+        <Text as="h2">404 — Page not found</Text>
+        <Text as="p">The page you are looking for does not exist.</Text>
+        <NextLink
+          className={text({ fontSize: 'bodyLarge', textColor: 'Black' })}
+          type="link"
+          href="/"
+        >
+          Go back home
+        </NextLink>
+      </div>
     </div>
   );
 }
