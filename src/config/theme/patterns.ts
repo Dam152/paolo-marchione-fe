e83d8b80dc7f3ff5df12f957015aadecbbe5f5ca
Damiano Ciucci <damjaguar98@icloud.com>
2026-03-12 16:45:21 +0100

    feat: update maxWidth values for container pattern to improve layout consistency

--- src/config/theme/patterns.ts
+++ src/config/theme/patterns.ts
@@ -13,9 +13,9 @@ export const container = definePattern({
       maxWidth: fluid
         ? '100%'
         : {
-            base: 'min(1360px, calc(100% - 32px))',
-            md: 'min(1360px, calc(100% - 48px))',
-            lg: 'min(1360px, calc(100% - 64px))',
+            base: 'min(1048px, calc(100% - 32px))',
+            md: 'min(1048px, calc(100% - 48px))',
+            xl: 'min(1048px, calc(100% - 64px))',
             '2xl': 'min(1360px, calc(100% - 96px))',
           },
       mx: 'auto',
